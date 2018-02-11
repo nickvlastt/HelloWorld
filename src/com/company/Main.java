8f388f0e3701d710a812e63fa2d08633deb70e75
nickvlastt <vlastt@gmail.com>
2018-02-11 23:21:51 +0100

    Add line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,10 @@ public class Main
 {
     public static void main(String[] args)
     {
-       System.out.println("Hello World!");
-       System.out.println ("Line");
+        byte n = 23;
+        System.out.println("Hello World!");
+        System.out.println("Line");
+        System.out.println(n);
 
     }
 }
