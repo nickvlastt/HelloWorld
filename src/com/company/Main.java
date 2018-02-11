5bbc175f48b56182ab4619c985d53b49aca0a47a
nickvlastt <vlastt@gmail.com>
2018-02-11 23:37:53 +0100

    Add line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,9 @@ public class Main
         String b = "short = -32768 до 32767";
         String c = "char = беззнаковое целое число, представляющее собой символ UTF-16 (буквы и цифры)";
         String d = "int = -2147483648 до 2147483647";
+        String e = "long = -9223372036854775808 до 9223372036854775807";
+        String f = "float =  -1.4e-45f до 3.4e+38f";
+        String g = "double = -4.9e-324 до 1.7e+308";
 
         System.out.println("Hello World!");
         System.out.println("Line");
@@ -15,7 +18,9 @@ public class Main
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
-
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
 
     }
 }
