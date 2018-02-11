3e16c503e84d37d36773469ee6568932b8ff2942
nickvlastt <vlastt@gmail.com>
2018-02-11 23:31:20 +0100

    Add line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,18 @@ public class Main
 {
     public static void main(String[] args)
     {
-        byte n = 23;
+        String a = "byte = -128 до 127";
+        String b = "short = -32768 до 32767";
+        String c = "char = беззнаковое целое число, представляющее собой символ UTF-16 (буквы и цифры)";
+        String d = "int = -2147483648 до 2147483647";
+
         System.out.println("Hello World!");
         System.out.println("Line");
-        System.out.println(n);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
 
     }
 }
